8d4151bbff2dc5ae8b064db651db10019818b69a
Jameka Echols <jameka.echols@gmail.com>
2019-03-05 10:56:00 -0600

    Set up files and linked them as well as did first practice

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("hi")
+/*
+Some software developers were contracted to build some code, but there weren't quite clear yet on how scope works. Your job is to fix their code to stop all the errors from being thrown.
+
+*/
+
+// ==================== Practice 1 ==========================
+/*
+C is for Cookie
+*/
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let x = 1; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
